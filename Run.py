8ca0653e59c3df74bab17708f0ec544029eaf3ed
AlarianxB <sebastianboteanu@gmail.com>
2018-01-20 00:41:53 +0200

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,16 @@
+from UI.Main3 import Ui_MainWindow
+from UI import Main3
+from PyQt5 import QtCore, QtGui  # @UnusedImport
+import sys
+class run():
+    def start(self):
+     self.ex=Ui_MainWindow()
+     self.ex.show()
+
+
+
+if __name__ == '__main__':
+    app=QtGui.QApplication(sys.argv)
+    r=run()
+    r.start()
+    sys.exit(app.exec_())
